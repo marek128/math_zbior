cba96b15f3fe297cd86f19268bc59a77fbbe1de9
marek128 <marek.m128@interia.pl>
2013-08-19 21:54:59 +0200

    modul konta

---
+++ MathZbior/apps/frontend/modules/konta/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * konta actions.
+ *
+ * @package    MathZbior
+ * @subpackage konta
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class kontaActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+//     $this->forward('default', 'module');
+  }
+}
