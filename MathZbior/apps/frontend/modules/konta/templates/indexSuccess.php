5017e3cab0971e907f9d9a1dea04be032bf15f8f
marek128 <marek.m128@interia.pl>
2013-08-20 22:11:14 +0200

    kontroler

--- MathZbior/apps/frontend/modules/konta/templates/indexSuccess.php
+++ MathZbior/apps/frontend/modules/konta/templates/indexSuccess.php
@@ -1,5 +1,9 @@
-<?php
+<div id="main">
+  <div id="head">
+    <h2>Mat Zbiór</h2>
+  </div>
+  <?php include_partial('global/main_menu') ?>
+  <div id="content">Tutaj witaj user in math!</div>
+  <div id="footer">Stopka</div>
+</div>
 
-
-echo "Konta"
-?>
---
+++ MathZbior/apps/frontend/templates/_main_menu.php
@@ -0,0 +1,5 @@
+<div id="main_menu">
+<a href="<?php echo url_for('temat/szukaj')?>">Szukaj tematu</a>
+<a href="<?php echo url_for('temat/dodaj')?>">Dodaj temat</a>
+</ul>
+</div>
